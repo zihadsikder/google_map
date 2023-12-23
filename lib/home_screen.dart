777813df1544a6a734bfc468177a1e853dce87aa
zihadsikder <zihadsikder.cse@gmail.com>
2023-12-23 23:49:59 +0600

    update location

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,6 +10,58 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  double _lat = 0;
+  double _lon = 0;
+
+  @override
+  initState() {
+    super.initState();
+    initVales();
+  }
+
+  void _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    Position position = await Geolocator.getCurrentPosition();
+
+    setState(() {
+      _lat = position.latitude;
+      _lon = position.longitude;
+    });
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,27 +69,31 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Home'),
       ),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(
+        initialCameraPosition: CameraPosition(
           zoom: 17,
-          target: LatLng(23.792265005916146, 90.40561775869223),
+          target: LatLng(_lat, _lon),
           bearing: 0,
           tilt: 5,
         ),
-        onTap: (LatLng position){
+        onTap: (LatLng position) {
           print(position);
         },
-        onLongPress: (LatLng latLng){
-        print(latLng);
+        onLongPress: (LatLng latLng) {
+          print(latLng);
         },
-        onCameraMove: (cameraPosition){
+        onCameraMove: (cameraPosition) {
           print(cameraPosition);
         },
-        zoomControlsEnabled: false,
-        zoomGesturesEnabled: false,
-        compassEnabled: false,
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        compassEnabled: true,
         myLocationButtonEnabled: true,
         myLocationEnabled: true,
       ),
     );
+  }
+
+  void initVales() {
+    _determinePosition();
   }
 }
